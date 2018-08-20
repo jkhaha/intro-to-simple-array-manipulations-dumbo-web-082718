32d9036dbcf3f3a28bf85a496ec36daf99e4a718
Jackie Ha <jha2@wellesley.edu>
2018-08-20 10:23:42 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,8 @@ def using_concat(array)
 end
 
 def using_insert
+  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+    @another_esoteric_language = "Malbolge" at 4
 end
 
 def using_uniq
