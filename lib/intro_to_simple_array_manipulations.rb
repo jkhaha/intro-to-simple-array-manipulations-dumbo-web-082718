8596227ee5672f53db61ccd0b5003aab12855381
Jackie Ha <jha2@wellesley.edu>
2018-08-20 09:42:16 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-    next_country = "Niger"
+  countries_in_western_africa.push("Niger")
 end
 
 def using_unshift
