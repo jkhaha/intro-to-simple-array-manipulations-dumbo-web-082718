f4c93f832086a798ee61ad442b816b6faec8182e
Jackie Ha <jha2@wellesley.edu>
2018-08-20 09:54:38 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,8 +8,9 @@ def using_unshift(array, element)
    neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
-def using_pop
+def using_pop(array)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties.pop
 end
 
 def pop_with_args
